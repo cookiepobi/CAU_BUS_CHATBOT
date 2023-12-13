8513c5c4b7bc58b372d4c3915834a390e92f9056
leekyuri1217 <71971836+leekyuri1217@users.noreply.github.com>
2023-12-14 02:50:27 +0900

    Create application.py

---
+++ application.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+import sys
+app = Flask(__name__)
+
+@app.route('/image', methods = ['POST'])
+def image(): 
+    response = {
+    "version": "2.0",
+    "template": {
+        "outputs": [
+            {
+                "simpleImage": {
+                    "imageUrl": "https://i.ibb.co/M658vfX/image.png",
+                    "altText": "통학 셔틀 버스 시간표를 알려줍니다"
+                }
+            }
+        ]
+    }
+}
+    return jsonify(response)
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(sys.argv[1]), debug=True)
