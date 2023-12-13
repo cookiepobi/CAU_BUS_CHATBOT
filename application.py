5a703ffc7e14f1364383d2afd95e8fbe19dd8c9b
haeun <131110863+cookiepobi@users.noreply.github.com>
2023-12-13 14:35:56 +0900

    Add files via upload

---
+++ application.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+import sys
+application = Flask(__name__)
+
+@application.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@application.route("/name", methods = ["POST"])
+def name_function():
+    body = request.get_json()
+    print(body)
+    print(body['userRequest']['utterance'])
+    
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": "안녕하세요. 테스트 코드입니다. 😊"
+                    }
+                    
+                    # "simpleImage": {
+                    #     "imageUrl": "http://k.kakaocdn.net/dn/jur1c/btrN5wwGslk/RU9W2xYA4RiRfnafDmkbh1/resize.jpg",
+                    #     "altText": "봉추찜닭입니다"
+                    # }
+                }
+            ]
+        }
+	}
+    return responseBody
+
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', port=int(sys.argv[1]), debug = True)
